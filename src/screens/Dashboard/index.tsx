889ebeb9dd8d020da721f07d07d876bbb039cdce
Jaqueline Gonzaga <jaqueline_gonzaga@hotmail.com>
2022-07-13 21:12:03 -0400

    feat: rocket chapter II styled components & expo

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighLightCards,
+    Transactions,
+    Title,
+    TransactionList
+} from './styles';
+
+export interface DataListProps extends TransactionCardProps {
+    id: string
+}
+
+export function Dashboard() {
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Hamburgueria Pizze",
+            amount: "R$ 59,00",
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '3',
+            type: 'positive',
+            title: "Aluguel do apartamento",
+            amount: "R$ 1200,00",
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: "13/04/2020"
+        }
+    ];
+
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/82250199?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Jaqueline</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+            <HighLightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Entradas"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 a 16 de abril"
+                />
+            </HighLightCards>
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
+        </Container>
+    );
+}
